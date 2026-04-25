2573b2a4818de8f7bbcd275855f00d3eb87cd1a6
SANTANU NANDI <santanu2005nandi@gmail.com>
2026-04-25 12:18:39 +0530

    Add findByUser method to AdminRepository

--- backend/src/main/java/com/HackOverflow/backend/repository/AdminRepository.java
+++ backend/src/main/java/com/HackOverflow/backend/repository/AdminRepository.java
@@ -3,7 +3,8 @@ package com.HackOverflow.backend.repository;
 import com.HackOverflow.backend.model.AdminProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 @Repository
 public interface AdminRepository extends JpaRepository<AdminProfile, Long> {
+      Optional<AdminProfile> findByUser(Users user);
 }
