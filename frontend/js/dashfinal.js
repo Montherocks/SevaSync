9cce851146f8891ce4565643e46e9df1e37c3601
Montherocks <meghna.mukherjee2005@gmail.com>
2026-04-28 22:41:26 +0530

    Update dashfinal.js

--- frontend/js/dashfinal.js
+++ frontend/js/dashfinal.js
@@ -162,7 +162,7 @@ function setKPIData(data){
 
   // HARD-CODED DASHBOARD VALUES (FOR NOW)
   const hours = 2;
-  const pending = 3;
+  const pending = 4;
   const active = 2;
   const completed = 0;
 
@@ -211,7 +211,7 @@ async function loadDashboard() {
 
     setKPIData({
         volunteerHours: 2,
-        tasksPending: 3,
+        tasksPending: 4,
         tasksActive: 2,
         tasksCompleted: 0
     });
